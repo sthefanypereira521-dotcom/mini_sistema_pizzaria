06d2f149d4d2a9d15016a8c34942ae10e14c5aa1
sthefanypereira <sthefanypereira521@gmail.com>
2026-09-20 16:57:16 -0300

    feat: adiciona schemas da aplicacao

---
+++ schemas/schemas.py
@@ -0,0 +1,54 @@
+from pydantic import BaseModel
+from typing import Optional,List
+
+
+class UsuarioSchema(BaseModel):
+    nome: str
+    email: str
+    senha: str
+    ativo: Optional[bool]
+    admin: Optional[bool]
+
+
+class Config:
+    from_attributes = True
+
+
+class PedidoSchema(BaseModel):
+    id_usuario: int
+
+
+class Config:
+    from_attributes = True
+
+
+class LoginSchema(BaseModel):
+    email: str
+    senha: str
+
+
+class Config:
+    from_attributes = True
+
+
+class ItemPedidoSchema(BaseModel):
+    quantidade: int
+    sabor: str
+    tamanho: str
+    preco_unitario: float
+
+
+class Config:
+    from_attributes = True
+
+
+
+class ResponsePedidoDoSchema(BaseModel):
+    id: int
+    status: str
+    preco: float
+    itens: List[ItemPedidoSchema]
+
+
+class Config:
+    from_attributes = True
